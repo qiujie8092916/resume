dc6e9c9f5c5dbb6dab3e6039a12b342deb40daff
QiuJie <qiujieee_empty@outlook.com>
2016-07-13 23:09:27 +0800

    issue:scrolld.js

--- resume/js/index.js
+++ resume/js/index.js
@@ -1,6 +1,35 @@
 ﻿$(function(){
 		new WOW().init()
-		$("[id*='Btn']").stop(true).on('click', function (e){e.preventDefault();$(this).scrolld()})
+		$("[id*='Btn']").stop(true).on('click', function (e){
+			e.preventDefault()
+			$(this).scrolld({
+				callback: function(){
+					$(this).parent("li").parent("ul").children("li").each(function(){
+						if($(this).hasClass("custom_active")){
+							$(this).removeClass("custom_active")
+						}
+					})
+					$(this).parent("li").addClass("custom_active")
+				}
+			})
+		})
+		/*
+		$("[id*='Btn']").on('click', function (e){$(this).scrolld({
+			callback: function(){
+				$(this).parent("li").parent("ul").children("li").each(function(){
+					if($(this).hasClass("custom_active")){
+						$(this).removeClass("custom_active")
+					}
+				})
+				$(this).parent("li").addClass("custom_active")
+			}}
+		)})*/
+		
+		
+		$(".homebackground").css("height", $(window).height())
+		$(window).resize(function() {
+			$(".homebackground").css("height", $(window).height())
+		})
 		
 		$(".logo img").hover(function(){
 					$("#persion2").animate({opacity: "1"}, 600)
