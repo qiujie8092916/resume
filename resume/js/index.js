fe9f9c7343f47682ce9392efd57b5ce0b350e660
QiuJie <qiujieee_empty@outlook.com>
2016-07-13 16:25:27 +0800

    the first version resume for git

---
+++ resume/js/index.js
@@ -0,0 +1,17 @@
+﻿$(function(){
+		new WOW().init()
+		$("[id*='Btn']").stop(true).on('click', function (e){e.preventDefault();$(this).scrolld()})
+		
+		$(".logo img").hover(function(){
+					$("#persion2").animate({opacity: "1"}, 600)
+			}, function(){
+					$("#persion2").animate({opacity: "0"}, 600)
+			}
+		)
+		
+		$(".oneng").hover(function(){
+				$(this).text("Resume")
+		}, function(){
+				$(this).text("个人简历")
+		})
+})
