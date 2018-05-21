16d0f699652a09c539113a83bc5392d5f5952c13
qiujie <qiujieee_empty@outlook.com>
2018-05-22 02:48:34 +0800

    revert: _proversion

--- src/project_dao/resume/resume/js/index.js
+++ src/project_dao/resume/resume/js/index.js
@@ -6,8 +6,7 @@ import "@/resume/resume/js/jquery.slimscroll.js";
 import "@/resume/resume/js/jquery.fullPage.js";
 import "@/resume/resume/js/jquery.qtip.js";
 import "@/resume/resume/js/transition.js";
-const _prodVersion = "";
-const data = require("@/resume/resume/data.json?v=" + _prodVersion);
+const data = require("../data.json");
 const imgs = {
   brief: {
     html: require("../imgs/html.png"),
